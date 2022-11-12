d92e7bbedfa036e25cc5dad5d7886bf115da5ee4
Viktar <vchigir12@gmail.com>
2022-11-12 12:46:11 -0500

    Testing setup

--- tests/LexerTests/LexerTest.cpp
+++ tests/LexerTests/LexerTest.cpp
@@ -6,20 +6,24 @@
 
 using std::vector;
 using std::tuple;
+using std::cout;
+using std::endl;
 
 //tuple<string, vector<Token>>
 class LexerTestParametrizedFixture :public ::testing::TestWithParam<tuple<string, vector<Token>>> {
 protected:
-    Lexer lex;
+    Lexer lex = Lexer();
 };
 
 // Demonstrate some basic assertions.
 TEST_P(LexerTestParametrizedFixture, LexerAcceptsBasicTokens) {
   tuple<string, vector<Token>> inputs = GetParam();
   lex.setFilePath(get<0>(inputs));
+
   vector<Token> actual = lex.tokenizeFile();
   vector<Token> expected = get<1>(inputs);
-  EXPECT_EQ(expected, actual);
+
+  EXPECT_EQ(actual, expected);
 }
 
 INSTANTIATE_TEST_CASE_P(
@@ -27,7 +31,7 @@ INSTANTIATE_TEST_CASE_P(
       LexerTestParametrizedFixture,
       ::testing::Values(
               tuple<string, vector<Token>>{
-                "./TestPrograms/singleTokens.pl", 
+                "singleTokens.pl", 
                 vector<Token>{
                   Token("#keywords#", COMMENT_TKN, 1, 1),
                   Token("if", KEYWORD_TKN, 2, 1)
